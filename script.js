dcdb9f0147a58f9ea208c0edd5e60fd8e81e2ef3
emily <emil7809@gmail.com>
2021-09-07 21:28:27 +0200

    construction of code

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.addEventListener(DOMException, start);
+
+function start(){
+    document.querySelector("#color_selector").addEventListener("input", chosenColor);
+}
+
+function chosenColor() {
+  
+}
+
+function HEXvalues() {
+
+}
+
+function RGBvalues() {
+    
+}
+
+function HSLvalues() {
+    
+}
+
+function displayColor() {
+
+}
